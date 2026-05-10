eebd003094d599d58780adf7f45f6d466922faf5
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-05-10 16:33:01 +0800

    Add Library class with load, save, add_book, and _next_id

--- library-system/library_system.py
+++ library-system/library_system.py
@@ -1,3 +1,5 @@
+import json
+
 class Book:
     def __init__(self, number, title, author, genre, year, availability=True):
         self.id = number
@@ -32,4 +34,39 @@ class Book:
     @classmethod
     def from_dict(cls, data):
         return cls(data["id"], data["title"], data["author"], data["genre"], data["year"], data["availability"])
+
+class Library:
+    def __init__(self, filepath):
+        self.books = []
+        self.filepath = filepath
+        self.load()
+
+    def load(self):
+        try:
+            with open (self.filepath, "r") as f:
+                data = json.load(f)
+                self.books = [Book.from_dict(item) for item in data]
+
+        except (FileNotFoundError, json.JSONDecodeError):
+            self.books = []  
+
+    def save(self):
+        data = [book.to_dict() for book in self.books]
+        with open (self.filepath, "w") as f:
+            json.dump(data, f)
+
+    def _next_id(self):
+        if not self.books:
+            return 1
+        
+        else:
+            result = max(self.books, key=lambda book: book.id)
+            next_id = result.id + 1
+            return next_id
+        
+    def add_book(self, title, author, genre, year):
+        next_id = self._next_id()
+        book = Book(next_id, title, author, genre, year)
+        self.books.append(book)
+        self.save()
 
